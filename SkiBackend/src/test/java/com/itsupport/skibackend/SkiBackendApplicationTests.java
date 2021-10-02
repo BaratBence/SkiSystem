bed65cf189e72df74e352bee448269b49f36021c
Qpica <cs.k.kenez@gmail.com>
2021-10-02 14:42:07 +0200

    Added Maven Spring Project Added SkiBackend

---
+++ SkiBackend/src/test/java/com/itsupport/skibackend/SkiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsupport.skibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
